7522bc26027d35527d89afb57c707cb602fc11ba
Ademir Júnior <adejr0220@gmail.com>
2024-12-01 20:36:00 -0300

    :sparkles: feat : READ Products
    Implementa método GET e lista produtos com todas as informações presentes no banco de dados.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './global.css'
-import App from './App.tsx'
+import ListProducts from './components/ListProducts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListProducts />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
