f222d2fe2027ef758cc06ebee4224ec2c3dd13ed
Ademir Júnior <adejr0220@gmail.com>
2024-12-04 10:39:52 -0300

    :iphone: :lipstick: style : mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { FaArrowRight, FaPlus } from "react-icons/fa"
+import { FaXmark } from "react-icons/fa6"
+import { MdOutlineMenuOpen } from "react-icons/md"
+import { PiHouseBold } from "react-icons/pi";
+import { Link, Outlet } from "react-router-dom"
+
 function App() {
-  return (
-    <main className='w-screen h-screen flex flex-col items-center justify-center'>
-      <h1>W.I.P</h1>
-      <p>Esta página está em desenvolvimento =)</p>
-    </main>
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => setOpen(!open);
+
+  return ( // TODO: refactoring (break into smaller components)
+    <>
+      <div className="absolute">
+        <div onClick={toggleMenu} className="absolute top-4 bg-orange text-white text-2xl w-fit p-2 rounded-e-md shadow-sm shadow-gray-500">
+          <MdOutlineMenuOpen />
+        </div>
+        <div className={`absolute z-20 flex flex-col gap-8 p-2 bg-orange shadow-md shadow-gray-500 w-[90vw] h-screen transition-all duration-500 ${open ? "" : "-translate-x-[90vw]"}`}>
+          <FaXmark className="text-4xl text-white self-end" onClick={toggleMenu} />
+          <ul className="flex flex-col gap-4 ">
+            <li className="w-full border-2 bg-white text-brown shadow-custom-01 border-white rounded-md p-3">
+              <Link to="/" onClick={toggleMenu} className="flex items-center gap-4 font-bold">
+                <PiHouseBold className="text-xl" /> Estoque
+              </Link>
+            </li>
+            <li className="w-full border-2 bg-white text-brown shadow-custom-01 border-white rounded-md p-3">
+              <Link to="/novo" onClick={toggleMenu} className="flex items-center gap-4 font-bold">
+                <FaPlus className="text-lg" /> Cadastrar Novo Produto
+              </Link>
+            </li>
+            <li className="w-full border-2 bg-white text-brown shadow-custom-01 border-white rounded-md p-3">
+              <Link to="/relatório" onClick={toggleMenu} className="flex items-center gap-4 font-bold">
+                <FaArrowRight className="text-lg" /> Gerar Relatório de Vendas
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Outlet />
+    </>
   )
 }
 
--- frontend/src/components/list-products/ListProducts.tsx
+++ frontend/src/components/list-products/ListProducts.tsx
@@ -1,19 +1,48 @@
 import { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { CiSearch } from 'react-icons/ci';
+import { TiThSmallOutline } from 'react-icons/ti';
+import { MdOutlineBakeryDining } from 'react-icons/md';
+import { RiCake3Line } from 'react-icons/ri';
+import { PiBreadFill } from 'react-icons/pi';
 
 type Product = {
   id: number;
   nome: string;
   categoria: string;
+  descricao: string;
   preco: number;
+  img?: string;
+  imgAlt?: string;
 };
+
+
 
 export default function ListProducts() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState('Todos');
+  const categories = [
+    {
+      name: 'Todos',
+      icon: <TiThSmallOutline />,
+    },
+    {
+      name: 'Pães',
+      icon: <PiBreadFill />,
+    },
+    {
+      name: 'Doces',
+      icon: <RiCake3Line />,
+    },
+    {
+      name: 'Salgados',
+      icon: <MdOutlineBakeryDining />,
+    },
+  ]
   const navigate = useNavigate();
 
-  useEffect(() => {
+  useEffect(() => { // TODO: refactoring (SOLID principles)
     axios.get('http://localhost:8000/api/produtos')
       .then((res) => {
         const response = res.data;
@@ -24,35 +53,65 @@ export default function ListProducts() {
       });
   }, []);
 
-  const handleDelete = async (id: number) => {
+  const handleDelete = async (id: number) => { // TODO: refactoring (SOLID principles)
     if (confirm('Deseja realmente excluir esse produto?')) {
       await axios.delete(`http://localhost:8000/api/produtos/${id}`);
       setProducts((prev) => prev.filter((product) => product.id !== id));
     }
   };
 
-  return (
-    <>
-      <div className='mb-4'>
-        <h1>Lista de Produtos</h1>
-        <Link to="/novo">Cadastrar Novo Produto</Link>
+  return (// TODO: refactoring (break into smaller components)
+    <main className='px-4 py-4 h-screen overflow-hidden'>
+      <h1 className='font-pacifico text-orange text-center mb-6'>Estoque</h1>
+      <span className='flex gap-2 shadow-custom-01 rounded-md px-4 py-3 bg-white mb-6'>
+        <input type="text" placeholder='Pesquisar por nome' className='w-full outline-none' />
+        <CiSearch className='text-2xl' />
+      </span>
+      <div className='flex flex-col gap-4 items-center'>
+        <p className='font-pacifico text-orange text-2xl self-start'>
+          Categorias: Todos
+        </p>
+        <div className='flex items-center justify-between w-full'>
+          {categories.map((category) => (
+            <button key={category.name} onClick={() => setSelectedCategory(category.name)} className={`flex items-center gap-2 p-4 rounded-full border-2 border-white text-2xl shadow-custom-01 ${selectedCategory === category.name ? "bg-orange text-white" : "bg-white text-brown"}`}>
+              {category.icon}
+            </button>
+          ))}
+        </div>
+        <small><i>Toque no produto para ver mais detalhes</i></small>
+        <hr className='w-full border-2 border-orange mb-4' />
       </div>
-      <ul className='flex flex-col gap-2 h-[70vh] overflow-y-scroll p-2'>
+      <ul className='flex flex-col gap-5 h-[302px]  overflow-y-scroll px-3 py-2 pb-4'>
         {products.map((product) => (
-          <li key={product.id} className='p-2 bg-gray-900 rounded-md flex items-center justify-between'>
-            <div>
-              <p className='font-bold text-xl'>{product.nome}</p>
-              <p>{product.categoria}</p>
-              <br />
-              <p className='font-bold text-xl'>R$ {product.preco.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 },)}</p>
-            </div>
-            <div className='flex flex-col gap-2'>
-              <button onClick={() => navigate(`/editar/${product.id}`)}>Editar</button>
-              <button onClick={() => handleDelete(product.id)}>Excluir</button>
+          <li key={product.id}>
+            <div className='bg-white h-full rounded-md overflow-hidden shadow-custom-01 flex items-center justify-between'>
+              <div className='w-[40%] h-full flex items-center justify-center bg-gray-300'>
+                {product.img ? (
+                  <img src={product.img} alt={product.imgAlt} />
+                ) : (
+                  <p className='text-4xl text-brown'>{product.nome[0]}</p>
+                )}
+              </div>
+              <div className='w-[60%] h-fit p-2'>
+                <p className='font-bold text-xl mb-2'>{product.nome}</p>
+                <p className='flex items-center gap-1 rounded-full bg-orange bg-opacity-70 w-fit px-2 mb-2'>
+                  {categories.find((category) => category.name === product.categoria)?.icon}
+                  {product.categoria}
+                </p>
+                <p><i>{product.descricao}</i></p>
+                <br />
+                <p className='font-bold text-xl'>
+                  <i>R$ {product.preco.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 },)}</i>
+                </p>
+              </div>
+              <div className='sm:flex flex-col gap-2 hidden'>
+                <button onClick={() => navigate(`/editar/${product.id}`)}>Editar</button>
+                <button onClick={() => handleDelete(product.id)}>Excluir</button>
+              </div>
             </div>
           </li>
         ))}
       </ul>
-    </>
+    </main>
   );
 }
