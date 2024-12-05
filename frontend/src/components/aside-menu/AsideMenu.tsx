f80e91539d04e755d6b4b6bc82c5718994ffd9e3
Ademir Júnior <adejr0220@gmail.com>
2024-12-05 15:14:21 -0300

    :iphone: fix visual break at ProductStock
    Arruma a largura da tela quando os produtos ainda não estão carregados; também arruma menu mobile (antes iniciava aberto, agora inicia fechado)

--- frontend/src/components/aside-menu/AsideMenu.tsx
+++ frontend/src/components/aside-menu/AsideMenu.tsx
@@ -16,7 +16,7 @@ export const AsideMenu = () => {
       <div onClick={toggleMenu} className="absolute top-6  bg-orange text-white text-3xl w-fit p-2 rounded-e-md shadow-sm shadow-gray-500 md:hidden">
         <MdOutlineMenuOpen />
       </div>
-      <div className={`absolute z-20 w-[90vw] shadow-xl shadow-gray-900 transition-all duration-500 ${open ? "-translate-x-[100vw]" : ""} md:fixed md:w-[32vw] md:translate-x-0 md:shadow-none flex flex-col gap-8 p-2 bg-orange h-screen`}>
+      <div className={`absolute z-20 w-[90vw] shadow-xl shadow-gray-900 transition-all duration-500 ${open ? "" : "-translate-x-[100vw]"} md:fixed md:w-[32vw] md:translate-x-0 md:shadow-none flex flex-col gap-8 p-2 bg-orange h-screen`}>
         <FaXmark className="text-4xl text-white self-end md:hidden" onClick={toggleMenu} />
         <nav className="py-8 px-4">
           <h1 className="text-white font-pacifico mb-10">Padaria Web</h1>
