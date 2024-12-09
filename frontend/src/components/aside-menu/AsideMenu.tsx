a4adae28a60b210db548b28aebe3149bb3f27c32
Ademir Júnior <adejr0220@gmail.com>
2024-12-09 18:33:11 -0300

    :lipstick: adjust aside-menu links clickable area

--- frontend/src/components/aside-menu/AsideMenu.tsx
+++ frontend/src/components/aside-menu/AsideMenu.tsx
@@ -21,18 +21,18 @@ export const AsideMenu = () => {
         <nav className="py-8 px-4">
           <h1 className="text-white font-pacifico mb-10">Padaria Web</h1>
           <ul className="flex flex-col gap-4">
-            <li className={`${at === "/" ? "bg-orange text-white" : "bg-white text-brown"} w-full border-[3px] border-white rounded-full p-3`}>
-              <Link to="/" onClick={toggleMenu} className="flex items-center gap-4 font-bold">
+            <li className={`${at === "/" ? "bg-orange text-white" : "bg-white text-brown"} w-full border-[3px] border-white rounded-full`}>
+              <Link to="/" onClick={toggleMenu} className="flex items-center gap-4 font-bold p-3">
                 <PiHouseBold className="text-xl" /> Estoque
               </Link>
             </li>
-            <li className={`${at === "/novo" ? "bg-orange text-white" : "bg-white text-brown"} w-full border-[3px] border-white rounded-full p-3`}>
-              <Link to="/novo" onClick={toggleMenu} className="flex items-center gap-4 font-bold">
+            <li className={`${at === "/novo" ? "bg-orange text-white" : "bg-white text-brown"} w-full border-[3px] border-white rounded-full`}>
+              <Link to="/novo" onClick={toggleMenu} className="flex items-center gap-4 font-bold p-3">
                 <FaPlus className="text-lg" /> Cadastrar Novo Produto
               </Link>
             </li>
-            <li className={`${at === "/relatorio" ? "bg-orange text-white" : "bg-white text-brown"} w-full border-[3px] border-white rounded-full p-3`}>
-              <Link to="/relatorio" onClick={toggleMenu} className="flex items-center gap-4 font-bold">
+            <li className={`${at === "/relatorio" ? "bg-orange text-white" : "bg-white text-brown"} w-full border-[3px] border-white rounded-full`}>
+              <Link to="/relatorio" onClick={toggleMenu} className="flex items-center gap-4 font-bold p-3">
                 <FaArrowRight className="text-lg" /> Gerar Relatório de Vendas
               </Link>
             </li>
