4203bd4e471364c9c917e9610f0ad17f7e2beb66
Ademir Júnior <adejr0220@gmail.com>
2024-12-11 12:03:43 -0300

    :lipstick: style: update rounded shape and improved responsiveness

--- frontend/src/components/buttons/Button.tsx
+++ frontend/src/components/buttons/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ type, children, onClick }: ButtonProps) => {
     <button
       type={type}
       onClick={onClick}
-      className='bg-orange text-white text-lg font-bold rounded-md px-4 py-2'
+      className='bg-orange text-white text-lg font-bold rounded-full px-4 py-2 min-w-[300px] transition-all active:scale-90 active:shadow-none'
     >
       {children}
     </button>
--- frontend/src/components/inputs/InputField.tsx
+++ frontend/src/components/inputs/InputField.tsx
@@ -10,7 +10,7 @@ type InputFieldProps = {
 
 export const InputField = ({ label, input, error, vertical }: InputFieldProps) => {
   return (
-    <div >
+    <div className="w-full">
       <span className={`${vertical ? " flex-col" : "flex-row"} flex gap-2 rounded-md shadow-custom-01 bg-white px-4 py-3`}>
         <label className='w-fit font-bold'>{label}:</label>
         {input}
