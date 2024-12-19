d0bfed814aaebc4b211755f28244964dfdc2b219
Ademir Júnior <adejr0220@gmail.com>
2024-12-18 21:01:52 -0300

    :recycle: refactoring: create SharedContext for both create and edit product forms

---
+++ frontend/src/contexts/shared-context/SharedContextProvider.tsx
@@ -0,0 +1,59 @@
+import { ReactNode, useState } from 'react';
+import { SharedContext } from './SharedContext';
+import { useWarnings } from '../../hooks/useWarnings';
+import { useProductData } from '../../hooks/useProductData';
+
+export const SharedContextProvider = ({ children }: { children: ReactNode }) => {
+  const {
+    openAlert,
+    setOpenAlert,
+    openConfirm,
+    setOpenConfirm,
+    openError,
+    setOpenError,
+    errorMessage,
+    setErrorMessage,
+  } = useWarnings();
+  const {
+    price,
+    setPrice,
+    productRequest,
+    setProductRequest,
+    productResponse,
+    setProductResponse,
+    handleNameChange,
+    handlePriceChange,
+    handleCategoryChange,
+    handleDescriptionChange,
+  } = useProductData();
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
+
+  return (
+    <SharedContext.Provider value={{
+      openAlert,
+      setOpenAlert,
+      openConfirm,
+      setOpenConfirm,
+      openError,
+      setOpenError,
+      errorMessage,
+      setErrorMessage,
+
+      price,
+      setPrice,
+      productRequest,
+      setProductRequest,
+      productResponse,
+      setProductResponse,
+      handleNameChange,
+      handlePriceChange,
+      handleCategoryChange,
+      handleDescriptionChange,
+
+      pageLoading,
+      setPageLoading,
+    }}>
+      {children}
+    </SharedContext.Provider>
+  );
+}
