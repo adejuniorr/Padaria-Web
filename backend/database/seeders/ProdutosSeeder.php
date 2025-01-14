c68deb585c75fb424b6be54bb28c65bda0c2e505
Ademir Júnior <adejr0220@gmail.com>
2025-01-14 11:42:56 -0300

    :arrow_up: update [backend]: Update database seeders

--- backend/database/seeders/ProdutosSeeder.php
+++ backend/database/seeders/ProdutosSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Produto;
 
-class ProdutoSeeder extends Seeder
+class ProdutosSeeder extends Seeder
 {
     /**
      * Run the database seeds.
